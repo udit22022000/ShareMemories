ebf337a01945f9a75580f23d6e16d2293b00cb28
udit22022000 <ping2udit@gmail.com>
2021-10-01 12:30:37 +0530

    added auth actions

---
+++ client/src/actions/auth.js
@@ -0,0 +1,28 @@
+import { AUTH } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+export const signin = (formData, router) => async (dispatch) => {
+  try {
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
+    router.push('/');
+  } catch (error) {
+    window.alert("Wrong Id or Password");
+    console.log(error);
+  }
+};
+
+export const signup = (formData, router) => async (dispatch) => {
+  try {
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
+    router.push('/');
+  } catch (error) {
+    window.alert("Wrong Password, Please Enter it correctly");
+    console.log(error);
+  }
+}; 
